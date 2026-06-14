ac0af6868c1c0bf59755c5e003dab47d78a8d57d
mthw07 <mbystritskiy@edu.hse.ru>
2026-06-14 16:18:02 +0000

    Commands for data_analyser in 'run.sh' added

--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ elif [ "$1" == "run_generator" ]; then
     docker run --rm -v "$(pwd)/data:/data" generator_image
 elif [ "$1" == "create_local_data" ]; then
     python ./data_generator/generate.py local_data
+elif [ "$1" == "build_reporter" ]; then
+    docker build -t analyser_image ./data_analyser
+elif [ "$1" == "run_reporter" ]; then
+    docker run --rm -v "$(pwd)/data:/data" analyser_image
 fi
