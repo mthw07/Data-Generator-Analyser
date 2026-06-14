c7cee38d071c00105c9310f39da0fd4b6f955985
mthw07 <mbystritskiy@edu.hse.ru>
2026-06-14 11:26:19 +0000

    Columns with proper names added

--- data_generator/generate.py
+++ data_generator/generate.py
@@ -6,15 +6,15 @@ import sys
 NUM_ROWS = 50
 
 
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["ALBUM", "ARTIST", "GENRE", "RELEASE YEAR"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "ALBUM": random.choice(["Abbey Road", "Led Zeppelin IV", "The Dark Side of the Moon", "Sticky Fingers", "A Night at the Opera", "The Rise and Fall of Ziggy Stardust and the Spiders from Mars", "Nevermind", "In the Court of the Crimson King", "Close to the Edge", "Selling England by the Pound", "L.A. Woman", "Are You Experienced", "Rumours", "Who's Next", "Paranoid", "Back in Black", "Master of Puppets", "Ten", "Superunknown", "Disintegration", "Violator", "Power, Corruption & Lies", "Unknown Pleasures", "The Queen Is Dead", "Automatic for the People", "The Joshua Tree", "London Calling", "Rocket to Russia", "Elephant", "The Colour and the Shape", "Dookie", "Octopus", "Moving Pictures", "Synchronicity", "White Light/White Heat", "Remain in Light"]),
+        "ARTIST": random.choice(["The Beatles", "Led Zeppelin", "Pink Floyd", "The Rolling Stones", "Queen", "David Bowie", "Nirvana","King Crimson", "Yes", "Genesis", "The Doors", "Jimi Hendrix", "Fleetwood Mac", "The Who", "Black Sabbath", "AC/DC", "Metallica", "Pearl Jam", "Soundgarden", "The Cure", "Depeche Mode", "New Order", "Joy Division", "The Smiths", "R.E.M.", "U2", "The Clash", "The Ramones", "The White Stripes", "Foo Fighters", "Green Day", "Gentle Giant", "Rush", "The Police", "The Velvet Underground", "Talking Heads"]),
+        "GENRE": random.choice(["Classical Rock", "Progressive Rock", "Jazz Rock", "Pop Rock", "Electonic Prog", "New Wave", "Folk Rock", "Hard Rock", "Heavy Metal", "Psychedelic Rock", "Glam Rock", "Art Rock", "Symphonic Prog", "Experimental Rock", "Avant-Garde", "Funk Rock", "Indie Rock", "Alternative Rock", "Post-Rock", "Shoegaze", "Dream Pop", "Ambient", "Industrial Rock", "Noise Rock", "Post-Punk", "Garage Rock", "Soft Rock", "Rock & Roll", "Blues Rock", "Southern Rock", "Country Rock", "Surf Rock", "Rockabilly", "Punk Rock", "Grunge", "Britpop", "Emo", "Pop Punk", "Ska Punk", "Hardcore Punk", "Post-Hardcore", "Math Rock"]),
+        "RELEASE YEAR": random.randint(1960, 1999),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
